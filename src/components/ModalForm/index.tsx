53873bd88604a4182f1cb086843998e982077ac3
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-09-24 16:38:23 -0300

    update layout modal

--- src/components/ModalForm/index.tsx
+++ src/components/ModalForm/index.tsx
@@ -1,8 +1,11 @@
+import { IoClose } from "react-icons/io5";
+
 export function ModalForm(){
     return(
         <div>
             <main className="flex justify-center">
                     <div className="absolute bg-[#020817] w-2xl flex flex-col border border-gray-100/20 top-1/2 + -translate-y-1/2 p-5.5 rounded-lg">
+                        <button className="cursor-pointer"><IoClose className="text-gray-100/60 text-2xl absolute right-5 top-4"/></button>
                         <h2 className="text-white text-lg font-bold">Criar Novo Registro</h2>
                         <p className="text-gray-100/60 mb-5.5">Preencha os campos para criar um novo registro de expedição.</p>
                         <form className="flex flex-col">
@@ -26,9 +29,9 @@ export function ModalForm(){
                             </div>
                             <div className="flex flex-col">
                                 <label className="text-white">Observações</label>
-                                <textarea rows={5} className="border border-gray-100/20 rounded-lg focus:border-green-100/40 outline-none"></textarea>
+                                <textarea rows={5} className="border border-gray-100/20 rounded-lg focus:border-green-100/40 outline-none text-white p-3.5"></textarea>
                             </div>
-                            <button className="text-[#000] bg-[#6F5AF5] mt-4 p-2 rounded-lg cursor-pointer">Criar registro</button>
+                            <button className="text-[#000] bg-[#6f5af5d7] mt-4 p-2 rounded-lg cursor-pointer hover:bg-[#6F5AF5] hover:brightness-125 duration-500">Criar registro</button>
                         </form>
                     </div>
             </main>
