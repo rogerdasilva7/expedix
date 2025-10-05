a25cdd9df159d73c3b531720d30bf85d781d060b
rogerdasilva7 <rogerdasilvacode7@gmail.com>
2025-10-05 14:18:25 -0300

    perf: change in sidebar logic

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -26,15 +26,22 @@ function sidebarCollapsed(){
 }
     return(
         <div>
-            <main className={`${!isCollapsed ? "w-64" : "w-20"} h-screen bg-[#020817] flex flex-col border-solid border-r-1 border-gray-100/20 transition-all duration-700`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+            <main className={`${!isCollapsed ? "w-64" : "w-20"} h-screen bg-[#020817] flex flex-col border-solid border-r-1 border-gray-100/20 transition-all ease-in-out duration-300`}
+            onMouseEnter={() => {
+                if(isCollapsed) setIsHovered(true);
+            }}
+            onMouseLeave={() => {
+                if(isCollapsed) setIsHovered(false)
+            }}
+            >
                 <nav className="text-white pr-5 pl-5 flex flex-col h-screen justify-between">
                     <div>
                         <div className="flex items-center ml-2 mt-5 relative">
-                            {isHovered ? <button onClick={() => setIsCollapsed(false)}><div className="hover:text-[#6F5AF5] hover:bg-[#6f5af511] rounded-lg duration-500 ease-in-out cursor-pointer"><PiSidebarDuotone className="absolute right-0.5 text-2xl top-2"/></div></button> : <img src={logo} alt="Expedix logo" className="h-8 w-8"/>}
+                            {isHovered ? <button  className="h-8" onClick={() => setIsCollapsed(false)}><div className="hover:text-[#6F5AF5] hover:bg-[#6f5af511] rounded-lg duration-500 ease-in-out cursor-pointer"><PiSidebarDuotone className="absolute right-0.5 text-2xl top-1"/></div></button> : <img src={logo} alt="Expedix logo" className="h-8 w-8"/>}
                             {!isCollapsed && <h1 className="text-2xl font-bold ml-2.5">Expedix</h1>}
                             <button className="cursor-pointer" onClick={() => sidebarCollapsed()}>
                                 <div className="hover:text-[#6F5AF5] hover:bg-[#6f5af511] rounded-lg duration-500 ease-in-out cursor-pointer">
-                                    {!isCollapsed && <PiSidebarDuotone className="absolute right-0.5 text-lg top-2"/>}
+                                    {!isCollapsed && <PiSidebarDuotone className="absolute right-0.5 text-2xl top-1"/>}
                                 </div>
                             </button>
                         </div>
